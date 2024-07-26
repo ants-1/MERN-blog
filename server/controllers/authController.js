0a71211d33647c5738cc277375b89f15e533d8b1
Anthony <anthonysalisbury98@outlook.com>
2024-07-26 22:18:47 +0100

    Add endpoints to get and update user profile

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,7 @@ const asyncHandler = require("express-async-handler");
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const verifyToken = require("../config/verifyToken");
 
 exports.sign_up = asyncHandler(async (req, res, next) => {
   const existingUser = await User.findOne({ username: req.body.username });
@@ -44,7 +45,7 @@ exports.login = [
       return res.status(200).json({
         message: "Authentication successful",
         token: token,
-        expiresIn: "30m"
+        expiresIn: "30m",
       });
     });
   }),
@@ -59,10 +60,38 @@ exports.logout = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.get_profile = asyncHandler(async (req, res, next) => {
+exports.get_profile = [
+  verifyToken,
+  asyncHandler(async (req, res, next) => {
+    const { userId } = req.params;
+    const user = await User.findById(userId).select("-password");
 
-});
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
 
-exports.update_profile = asyncHandler(async (req, res, next) => {
+    res.status(200).json({ user });
+  }),
+];
 
-});
+exports.update_profile = [
+  verifyToken,
+  asyncHandler(async (req, res, next) => {
+    const { userId } = req.params;
+
+    const updatedUser = {
+      username: req.body.username,
+      email: req.body.email,
+    };
+
+    const user = await User.findByIdAndUpdate(userId, updatedUser, {
+      new: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    res.status(200).json(user);
+  }),
+];
